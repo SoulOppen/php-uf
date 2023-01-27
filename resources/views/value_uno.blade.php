7299fe090bab43a00792e2143df9832c865e7385
oppen <arieloppenheimer@outlook.cl>
2023-01-26 22:41:01 -0300

    actuli

--- resources/views/value_uno.blade.php
+++ resources/views/value_uno.blade.php
@@ -1,11 +1,7 @@
 @extends('plantilla')
 @section('content')
     <h1 class="my-3">Valor Uf </h1>
-    <h3 class="my-3">Fecha: <?php
-        $input = $value->fecha;
-        $date = strtotime($input);
-        echo date('d-m-y', $date);    
-    ?><h3>
+    <h3 class="my-3">Fecha: {{date('d/m/Y',strtotime($value->fecha))}}
     <h3 class="my-3">Valor: {{$value->valor}}<h3>
     <div class="container d-flex justify-content-around my-3">
         <a href="{{route('value_editar',$value)}}" class=" btn btn-warning ">Editar</a>
